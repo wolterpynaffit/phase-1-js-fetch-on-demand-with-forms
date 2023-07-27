8eb1d66a1f9e20f6a2ddf04cf2c3750d9be71c20
Tiffany Pretlow <wolterpynaffit@yahoo.com>
2023-07-26 21:38:38 -0400

    completed fetch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 const init = () => {
+    const inputForm = document.querySelector("form")
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+
+        const input = document.querySelector("input#searchByID");
+        console.log(input.value)
+
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            console.log(data.title)
+            console.log(data.summary)
+
+            const title = document.querySelector('h4')
+            const summary = document.querySelector('p')
+
+            title.textContent = data.title
+            summary.textContent = data.summary
+
+        })
+
+    })
   
 }
 
